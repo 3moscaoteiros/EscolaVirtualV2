19b52d3dcbee63e65725605fa01c785f0c892e40
Maxim Grebeniuk <ytmax180@gmail.com>
2022-10-19 21:12:27 +0100

    .

--- ProjetoEscola/ProjetoEscola/AdminForm.cs
+++ ProjetoEscola/ProjetoEscola/AdminForm.cs
@@ -40,7 +40,20 @@ namespace ProjetoEscola
             }
             #endregion
 
+            #region update requests List
+            string request = "";
 
+
+            //maybe not ok
+            List<Student> RequestStudents= (Program.Anos.SelectMany(y => y.CLasses.ToList().SelectMany(c=>c.students.Where(s=>s.Request==true))).ToList());
+
+            foreach(Student st in RequestStudents)
+            {
+                request = $"name:{st.Name}, id:{st.ID}";
+                lstRequest.Items.Add(request);
+            }
+
+            #endregion
 
         }
 
@@ -104,99 +117,144 @@ namespace ProjetoEscola
 
         private void btnCreateStudent_Click(object sender, EventArgs e)
         {
-           
-            #region variables
-            string name = txtNameStudent.Text.Trim();
-            string num = txtNumStudent.Text.Trim();
-            string nif = txtNIFStudent.Text.Trim(); ;
-            string adress = txtAdressStudent.Text.Trim(); 
-            string contact = txtContactStudent.Text.Trim();
-            string Class = cbbClassStudent.SelectedItem.ToString();
-            string money = txtBalanceStudent.Text.Trim();
-            string pin = txtPINStudnet.Text.Trim();
-
-            #region errors
-            if( name == "" || num == "" || nif == "" || adress == "" || contact == "" || Class == "" || money==""  || pin =="" )
+            try
             {
-                MessageBox.Show("ERROR", "Information missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            #endregion
-            string YearWithTheClass = "";
-            #endregion
+                #region variables
+                string name = txtNameStudent.Text.Trim();
+                string num = txtNumStudent.Text.Trim();
+                string nif = txtNIFStudent.Text.Trim(); ;
+                string adress = txtAdressStudent.Text.Trim();
+                string contact = txtContactStudent.Text.Trim();
+                string Class = cbbClassStudent.SelectedItem.ToString();
+                string money = txtBalanceStudent.Text.Trim();
+                string pin = txtPINStudnet.Text.Trim();
+                bool exists = false;
+                string YearWithTheClass = "";
+                #endregion
 
-            Student student = new Student()
-            {
-                Name = name,
-                Adress = adress,
-                EMAIL = contact,
-                NIF = Convert.ToInt32(nif),
-                ID = num,
-                PIN = pin,
-                Request = false
-                
-            };
-
-            //percorrer anos para ver qual é o ano que tem a turma
-            foreach (Year y in Program.Anos)
-            {
-                foreach (Class c in y.CLasses)
+                #region errors
+                if (name == "" || num == "" || nif == "" || adress == "" || contact == "" || Class == "" || money == "" || pin == "")
                 {
-                    if (c.Name == Class)
-                        YearWithTheClass = y.year;
+                    MessageBox.Show("ERROR", "Information missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
                 }
-            }
+                #endregion
 
+                #region check if exists
+                //search in list if student exists
+                foreach (Year y in Program.Anos)
+                {
+                    foreach (Class c in y.CLasses)
+                    {
+                        foreach (Student s in c.students)
+                        {
+                            if (s.ID == num)
+                                exists = true;
+                        }
+                    }
+                }
+
+
+                //error
+                if (!exists)
+                {
+                    txtNumStudent.Text = "";
+
+                    MessageBox.Show("Wrong information!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                #endregion
+
+                #region ADD STUDENT TO CLASS AND THEN YEAR
+
+                Student student = new Student()
+                {
+                    Name = name,
+                    Adress = adress,
+                    EMAIL = contact,
+                    NIF = Convert.ToInt32(nif),
+                    ID = num,
+                    PIN = pin,
+                    Request = false
+
+                };
+
+                //cycle list years to see which is the year that has the student's class
+                foreach (Year y in Program.Anos)
+                {
+                    foreach (Class c in y.CLasses)
+                    {
+                        if (c.Name == Class)
+                            YearWithTheClass = y.year;
+                    }
+                }
+
+                //lambda to add the student
+                Program.Anos.Where(y => y.year == YearWithTheClass).FirstOrDefault().CLasses.Where(c=>c.Name==Class).FirstOrDefault().students.Add(student);
+                ////////////////////////
+                ////////////////////////
+                ////////////////////////
+                #endregion
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
 
 
         }
 
      
 
-
-
         private void btnCreateClass_Click(object sender, EventArgs e)
         {
-            string year = txtChooseYear.Text;
-            string Class = txtCreateClass.Text;
-
-            #region errors 
-            if (year.Trim() != "" || Class.Trim()=="")
+            try
             {
-                MessageBox.Show("ERROR","Info missing",MessageBoxButtons.OK,MessageBoxIcon.Error);
-                return;
-            }
+                string year = txtChooseYear.Text;
+                string Class = txtCreateClass.Text;
 
-            if (Convert.ToInt32(year.Trim()) < 7 || Convert.ToInt32(year.Trim()) >12)
+                #region errors 
+                if (year.Trim() != "" || Class.Trim() == "")
+                {
+                    MessageBox.Show("ERROR", "Info missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (Convert.ToInt32(year.Trim()) < 7 || Convert.ToInt32(year.Trim()) > 12)
+                {
+                    MessageBox.Show("ERROR", "Wrong year", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                #endregion
+
+                Class cl = new Class();
+                cl.Name = Class;
+                //why do I nedd class id?
+
+                #region verify if class already exists
+                //aux variable
+                bool hasClass = Program.Anos.Find(y => y.year == year).CLasses.Any(c => c.Name.Contains(Class));
+
+                
+                if (!hasClass)
+                {
+                    Program.Anos.Find(y => y.year == year).CLasses.Add(cl);//add the class to the chosen year
+
+                    //update cbb
+                    cbbClassStudent.Items.Add(cl.Name.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("ERROR", "Class already exists", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                #endregion
+            }
+            catch (Exception error)
             {
-                MessageBox.Show("ERROR", "Wrong year", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
+                MessageBox.Show(error.Message);
             }
-            #endregion
-
-            Class cl = new Class();
-            cl.Name = Class;
-            //why do I nedd class id?
-
-
-            //aux variable
-            bool hasClass = Program.Anos.Find(y => y.year == year).CLasses.Any(c => c.Name.Contains(Class));
-
-
-            //verify if class already exists
-            if (!hasClass)
-            {
-                Program.Anos.Find(y => y.year == year).CLasses.Add(cl);//add the class to the chosen year
-
-                //update cbb
-                cbbClassStudent.Items.Add(cl.Name.ToString());
-            }
-            else
-            {
-                MessageBox.Show("ERROR", "Class already exists", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
 
           
                    
--- ProjetoEscola/ProjetoEscola/Program.cs
+++ ProjetoEscola/ProjetoEscola/Program.cs
@@ -15,9 +15,7 @@ namespace ProjetoEscola
         };
 
 
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
